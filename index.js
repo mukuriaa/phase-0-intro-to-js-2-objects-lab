8832ca33a4aaba806518120c4e6d59c9a6236842
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-06 03:46:10 -0700

    commit tree e7d4809df5a341f0fd8b7e54070f4a9db444f2bc

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee ={
+    name:"Eric" ,
+    streetAdress:"Nairobi"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+   var newEmployee = {...employee}
+
+   newEmployee.name ="Sam";
+   newEmployee.streetAddress ="11 Broadway";
+
+   return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee.streetAddress ="12 Broadway"
+
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    var newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
